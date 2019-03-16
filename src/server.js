c362873b6fab94dc1fe1b783ab189963324b8132
Javier Ferrer <javilol@gmail.com>
2019-03-16 22:02:41 +0000

    rate limiting

--- src/server.js
+++ src/server.js
@@ -7,21 +7,40 @@ const axios = require('axios');
 // Define app.
 const server = express();
 const port = process.env.PORT;
+const rateLimitDelay = 2500;
 
 
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+const doRequest = function (url) {
+  return axios(url);
+}
+
+
+// TODO: Refactor logging.
+// TODO: Rate limiting?
 // GET - query.
 server.get('/query', async (req, res) => {
+  let data;
+
   try {
-    console.log('query:', req.query);
+    console.log('query:', req.query.address.substring(0, 80));
 
-    const doc = await axios(req.query.address);
+    doc = await doRequest(req.query.address);
+  } catch (e) {
+    console.log('e', e);
 
+    // TODO: Log errors!
+    res.status(400).send();
+  } finally {
     console.log('doc', doc.status);
 
     res.status(200).send(doc.data);
-  } catch (e) {
-    // TODO: Log errors!
-    res.status(400).send();
   }
 });
 
